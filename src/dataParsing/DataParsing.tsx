b03c77a6a3d88c03e4af406f99acf842b4d06462
Egor Dultsev <69510044+pluswhale@users.noreply.github.com>
2026-08-14 13:18:12 +0300

    Update DataParsing.tsx

--- src/dataParsing/DataParsing.tsx
+++ src/dataParsing/DataParsing.tsx
@@ -1,12 +1,16 @@
 import {useState,useEffect} from 'react'
 import {UserHooks} from '../hooks/UserHooksForParsing'
+// СamelCase, UserData 
 import {userData} from '../typesInterface/UserInterface'
 
+//Добавлять use если это хук 
 export function DataParsing(){
+    
     const[user, setUser] = useState<userData[]>([]) //массив обьектов
     const[waitingLoad, setWaitingLoad] = useState<boolean>(true) //ожидание HTTP ответа
     const [error, setError] = useState<null | string>(null)
     useEffect(() => {
+        //camelCase, parsingData 
         async function ParsingData() {
             try{
                 const user = await UserHooks() 
