710cfe42f409eb9fbdaf92529557b87cfd895068
Egor Dultsev <69510044+pluswhale@users.noreply.github.com>
2026-08-14 13:24:53 +0300

    Update UserHooksForParsing.tsx

--- src/hooks/UserHooksForParsing.tsx
+++ src/hooks/UserHooksForParsing.tsx
@@ -1,8 +1,22 @@
 import {userData} from '../typesInterface/UserInterface'
-export async function UserHooks(): Promise<userData[]> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    if(!response.ok){
-        throw new Error(`ошибка загрузки`)
+
+//Naming, useGetUser. 
+export async function UserHooks(): Promise<userData[] | null > {
+    // try catch - потенциально может стрельбнукт 
+    try {
+        
+        const response = await fetch('https://jsonplaceholder.typicode.com/users') 
+
+        if (!response.ok) {
+            throw new Error(`ошибка загрузки`)
+        }
+
+        //лишний await, проверить 
+        return(await response.json())   
+    } catch(e) {
+        logger(e, 'Cant fetch user')
+        return null;
     }
-    return(await response.json())
+
 }
+
