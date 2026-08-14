7644131bbafa0fcbc0f992445f197260dae45b5b
Egor Dultsev <69510044+pluswhale@users.noreply.github.com>
2026-08-14 13:15:33 +0300

    Update UserListData.tsx

--- src/components/UserListData.tsx
+++ src/components/UserListData.tsx
@@ -2,21 +2,26 @@ import {userData} from '../typesInterface/UserInterface'
 import {DataParsing} from '../dataParsing/DataParsing'
 import { UserItem } from "./UserItem";
 import {DataControls} from './DataControls'
+
 interface userListDataInterface {
     userArray: userData[], //т.к. filter/sort работает с масссивами
 }  
+
 export function UserListData(){
-    const {user,waitingLoad,error} = DataParsing()
-    if(waitingLoad){
+    //Необходимо будет пользолваться линтерами и prettier - нужно для корректного выраванивания кода.
+    const { user, waitingLoad, error } = DataParsing()
+    
+    if (waitingLoad) {
         return <h1 style = {{ color:'red' }}>ОЖИДАЙТЕ ЗАГРУЗКИ!</h1>
     }
-    if(error){
+    
+    if (error) {
         return <h1 style = {{ color:'red' }}>{error}</h1>
     }
+    
     return (
         <div>
             <DataControls userArray = {user} /> 
         </div>
-
     )
 }
