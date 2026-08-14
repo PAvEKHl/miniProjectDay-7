8f214fbe2bfc4695eb5e22c5cfe837dcb0db6236
Egor Dultsev <69510044+pluswhale@users.noreply.github.com>
2026-08-14 13:34:28 +0300

    Update DataParsing.tsx

--- src/dataParsing/DataParsing.tsx
+++ src/dataParsing/DataParsing.tsx
@@ -3,10 +3,11 @@ import {UserHooks} from '../hooks/UserHooksForParsing'
 // СamelCase, UserData 
 import {userData} from '../typesInterface/UserInterface'
 
-//Добавлять use если это хук 
-export function DataParsing(){
+//Добавлять use если это хук, useGetUser, return type(посмотри в доке)  
+export function DataParsing()  {
     
     const[user, setUser] = useState<userData[]>([]) //массив обьектов
+    //isLoading, setIsLoading
     const[waitingLoad, setWaitingLoad] = useState<boolean>(true) //ожидание HTTP ответа
     const [error, setError] = useState<null | string>(null)
     useEffect(() => {
@@ -15,15 +16,18 @@ export function DataParsing(){
             try{
                 const user = await UserHooks() 
                 setUser(user)
-                setWaitingLoad(false)
+                // error throw ->>>> 
+                setWaitingLoad(false) // потенциально не доходим до сюда = infinity loading 
             }
             catch(error){
                 setError("Ошибка HTTP запроса")
                 console.log(`ошибка ${error}`)
                 setWaitingLoad(false)
-            }
+            } finally {
+                setWaitingLoad(false)
+            } //использовать там где это возможно 
         }
-    ParsingData()
+        ParsingData()
     }, [])
     return ({user,waitingLoad,error}) //возврощаем состояния хуков
 }
